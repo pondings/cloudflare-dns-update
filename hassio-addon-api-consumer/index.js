49f1e3ec1d60ada1c029899e09f836f503100407
Pondwarut <pawarut.klaiarmon@gmail.com>
2024-03-22 13:26:44 +0700

    Cleaning repository

--- hassio-addon-api-consumer/index.js
+++
@@ -1,8 +0,0 @@
-// This file only serves to load the various api consumers.
-// Upload your consumers to the /share/consumers folder and define your consumers in the consumerList option by listing their filename minus the .js
-const options = require("./options");
-
-for (let i = 0 ; i < options.consumerList.length; i++) {
-   require('/share/consumers/' + options.consumerList[i]);
- }
- 
